262b020238837747350a0dff8693252cdd9bdbaa
guansaipeng <cgsp740178088@126.com>
2018-07-25 09:47:12 +0800

    '没什么事情，提交一次'

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
 import React, { Component } from 'react';
 // import Small, { big } from '@VProject/dashboard/small';
-import Big from '@VProject/dashboard/small';
+import Small from '@VProject/dashboard/small';
 
 class App extends Component {
   render() {
-    const obj = {
-      str: '失败',
+    const item = {
+      num: 12322222,
+      str: 'aaa11111'
     };
     return (
       <div className="app">
-        <Big item={obj} />
+        <Small {...item} />
       </div>
     );
   }
--- src/project-components/dashboard/small/index.jsx
+++ src/project-components/dashboard/small/index.jsx
@@ -1,21 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
-const big = ({ item }) => {
-  return (
-    <div>{item.str}</div>
-  );
+class Small extends Component {
+  constructor() {
+    super();
+    this.timer = null;
+    this.state = {
+      name: 'gsp',
+      sex: 'man'
+    };
+  }
+  render() {
+    if (!this.timer) {
+      this.timer = setTimeout(() => {
+        this.setState({
+          name: 'gsp111',
+          sex: 'mane111'
+        });
+      }, 1000);
+    }
+    console.log(this.props.num);
+    return (
+      <div>
+        <div>父组件传给我的 num是 {this.props.num}</div>
+        <div>父组件传给我的 str是 {this.props.str}</div>
+        <div>我自身的state中的name是 {this.state.name}</div>
+        <div>我自身的state中的sex是 {this.state.sex}</div>
+      </div>
+    );
+  }
+}
+
+Small.propTypes = {
+  num: PropTypes.number.isRequired,
+  str: PropTypes.string.isRequired
 };
 
-// class Small extends Component {
-//   render() {
-//     return (
-//       <div>
-//         我是很小
-//       </div>
-//     );
-//   }
-// }
-
-export default big;
-
-// export { big };
+export default Small;
