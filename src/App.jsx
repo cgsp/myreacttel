e12efa029a2d0c3d40e7a8a8849360483dcaea11
guansaipeng <cgsp740178088@126.com>
2018-07-25 14:24:15 +0800

    '投影插槽的使用'

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,17 @@
 import React, { Component } from 'react';
-// import Small, { big } from '@VProject/dashboard/small';
 import Small from '@VProject/dashboard/small';
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      num: 1,
-      str: 'a'
-    };
-    this.changeInputValue = this.changeInputValue.bind(this);
-  }
-  changeInputValue(num, str) {
-    console.log('父组件打印--子组件传给我的是:', num);
-    console.log('父组件打印--子组件传给我的是:', str);
-  }
-
-  changeSelf(item) {
-    console.log(item);
-  }
-
   render() {
-    const item = {
-      num: 12322222,
-      str: 'aaa11111',
-      changeInputValue: this.changeInputValue
-    };
-
-    /* eslint-disable */
     return (
       <div className="app">
-        <Small {...item} />
+        <Small>
+          <span>第一个节点</span>
+          <span>第二个节点</span>
+          <span>第3节点</span>
+        </Small>
       </div>
     );
-    /* eslint-enable */
   }
 }
 
--- src/project-components/dashboard/small/index.jsx
+++ src/project-components/dashboard/small/index.jsx
@@ -2,44 +2,18 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 class Small extends Component {
-  constructor() {
-    super();
-    this.timer = null;
-    this.state = {
-      name: 'gsp',
-      sex: 'man'
-    };
-  }
-  /* eslint-disable */
   render() {
-    if (!this.timer) {
-      this.timer = setTimeout(() => {
-        this.setState({
-          name: 'gsp111',
-          sex: 'mane111'
-        });
-      }, 1000);
-    }
-    console.log(this.props.num);
     return (
-      <div>
-        <div>父组件传给我的 num是 {this.props.num}</div>
-        <div>父组件传给我的 str是 {this.props.str}</div>
-        <div>我自身的state中的name是 {this.state.name}</div>
-        <div>我自身的state中的sex是 {this.state.sex}</div>
-        <div>
-          我是子页面的输入框 <input type="text" onChange={() => this.props.changeInputValue(12, 'aaa')} />
-        </div>
-      </div>
+      <ol>
+        {
+          React.Children.map(this.props.children, child => <li>{child}</li>)
+        }
+      </ol>
     );
   }
-  /* eslint-enable */
 }
-
 Small.propTypes = {
-  num: PropTypes.number.isRequired,
-  str: PropTypes.string.isRequired,
-  changeInputValue: PropTypes.func.isRequired
+  children: PropTypes.node,
 };
 
 export default Small;
