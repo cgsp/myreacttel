fac695e80825399d18000e7c58d16767bb7707b5
guansaipeng <cgsp740178088@126.com>
2018-07-25 16:28:46 +0800

    '提交一下，下班'

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,27 @@ import React, { Component } from 'react';
 import Small from '@VProject/dashboard/small';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      solider: [
+        '士兵1',
+        '士兵2',
+        '士兵3',
+      ]
+    };
+    this.addSolid = this.addSolid.bind(this);
+  }
+
+  addSolid() {
+    const length = this.state.solider.length;
+    this.setState({
+      solider: [...this.state.solider, `士兵${length + 1}`]
+    });
+  }
+
+
+
   render() {
     return (
       <div className="app">
@@ -10,6 +31,12 @@ class App extends Component {
           <span>第二个节点</span>
           <span>第3节点</span>
         </Small>
+        {
+          this.state.solider.map(v => (<li key={v}>{v}</li>))
+        }
+        <div>
+          <button onClick={this.addSolid}>新增士兵</button>
+        </div>
       </div>
     );
   }
