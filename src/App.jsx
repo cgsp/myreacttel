dbe742f4434f6741c3573843a021006b6faa266d
guansaipeng <cgsp740178088@126.com>
2018-08-03 12:01:12 +0800

    'redux开始'

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,10 @@
 import React, { Component } from 'react';
 import { Button } from 'antd-mobile';
+import { createStore } from 'redux';
 
 class App extends Component {
-  constructor() {
-    super();
-
-    this.state = {
-      soliders: ['虎子', '柱子']
-    };
-  }
   render() {
-    return (
-      <ul>
-        {this.state.soliders.map(solider => <li key={solider}>{solider}<Button type="primary" size="small" inline>small</Button></li>)}
-      </ul>
-    );
+
   }
 }
 
